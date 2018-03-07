a4607098677498edd7ffd787485c4d20b6dc9a50
Andrei F <soheevich@gmail.com>
2018-03-07 17:36:06 +0300

    Modify functions and image filenames

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,50 +3,10 @@
 (function autorun() {
   'use strict';
 
-  // HELPER FUNCTIONS
-
-  // Function to create DOM element
-  const createElement = (tag, attrs, ...children) => {
-    const element = document.createElement(tag);
-
-    Object.keys(attrs).forEach((key) => { element[key] = attrs[key]; });
-
-    children.forEach((child) => {
-      element.appendChild(child);
-    });
-
-    return element;
-  };
-
   // Make an array of cards' filenames.
   // This's an unnecessary step. All these names could be added manually.
   const createCardFilenames = () => {
     const extension = '.svg';
-    const array = [];
-    const suites = ['c', 'd', 'h', 's'];
-    suites.forEach((suite) => {
-      for (let i = 1; i < 14; i += 1) {
-        switch (i) {
-          case 1:
-            array.push(`1${suite}${extension}`);
-            break;
-          case 11:
-            array.push(`j${suite}${extension}`);
-            break;
-          case 12:
-            array.push(`q${suite}${extension}`);
-            break;
-          case 13:
-            array.push(`k${suite}${extension}`);
-            break;
-          default:
-            array.push(`${i}${suite}${extension}`);
-            break;
-        }
-      }
-    });
-
-    return array;
   };
 
   const model = (function modelAutorun() {
@@ -58,12 +18,54 @@
           scrset,
         };
       },
+
+      createCardNamesArray() {
+        return (function idArray() {
+          const array = [];
+          const suites = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+          suites.forEach((suite) => {
+            for (let i = 1; i < 14; i += 1) {
+              switch (i) {
+                case 1:
+                  array.push(`Ace-${suite}`);
+                  break;
+                case 11:
+                  array.push(`Jack-${suite}`);
+                  break;
+                case 12:
+                  array.push(`Queen-${suite}`);
+                  break;
+                case 13:
+                  array.push(`King-${suite}`);
+                  break;
+                default:
+                  array.push(`${i}-${suite}`);
+                  break;
+              }
+            }
+          });
+
+          return array;
+        }());
+      },
     };
   }());
 
   const view = (function viewAutorun() {
     return {
+      createElement(tag, attrs, ...children) {
+        const element = document.createElement(tag);
 
+        Object.keys(attrs).forEach((key) => {
+          element[key] = attrs[key];
+        });
+
+        children.forEach((child) => {
+          element.appendChild(child);
+        });
+
+        return element;
+      },
     };
   }());
 
@@ -74,8 +76,6 @@
     // ============
     const numberOfPairs = 9;
 
-    return {
-
-    };
+    return {};
   }());
 }());
